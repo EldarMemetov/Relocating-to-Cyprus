7f6a5843fc372cc1af3ea6897558b939c13be4d6
EldarMemetov <eldarm9347@gmail.com>
2023-08-21 13:36:30 +0300

    js

--- js/info.js
+++ js/info.js
@@ -1,11 +1,11 @@
 document.getElementById("button1").addEventListener("click", function () {
-  window.location.href = "../family.html";
+  window.location.href = "./family.html";
 });
 
 document.getElementById("button2").addEventListener("click", function () {
-  window.location.href = "../business.html";
+  window.location.href = "./business.html";
 });
 
 document.getElementById("button3").addEventListener("click", function () {
-  window.location.href = "../investment.html";
+  window.location.href = "./investment.html";
 });
